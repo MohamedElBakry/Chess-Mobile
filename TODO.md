f749f08bfd72b9acec4243d623d02cc821edaccb
Mohamed El Bakry <47481530+MohamedElBakry@users.noreply.github.com>
2021-04-08 10:46:26 +0100

    Pieces snap into grid locations based on where they are hovered

--- TODO.md
+++ TODO.md
@@ -1,13 +1,17 @@
 # TODO Chess Project
 
+##
+Factory sends Piece Array --> Cursor GO --> On input change --> main.script
+
+## TODO
 1. Change Background
 2. Implement piece click functionality
 3. Game Rules
 
-# Change Background
+## Change Background
 1. Open Game Art Background? Or Predownloaded background
 
-# Implement Piece Click Functionality
+## Implement Piece Click Functionality
 Store pieces in 2D array.
 On click, we have 2 options: 
 1. Convert the touch/click position to the array indices to find the piece
@@ -15,4 +19,4 @@ On click, we have 2 options:
 
 Which to do^^^^?
 
-# Game Rules
+## Game Rules
--- utils-module/utils.lua
+++ utils-module/utils.lua
@@ -3,11 +3,11 @@
 -- require "my_directory.my_file"
 -- in any script using the functions.
 
--- Translate the coordinates to array coordinates as their origins are mismatched
-local arr_coords = {8, 7, 6, 5, 4, 3, 2, 1}
-
 -- Set up a namespace
 utils = utils or {}
+
+-- Translate the coordinates to array coordinates as their origins are mismatched
+utils.arr_coords = {8, 7, 6, 5, 4, 3, 2, 1}
 
 -- Translate pixel coordinates to square x, y coordinates
 -- e.g. 800, 400
@@ -20,7 +20,7 @@ print("WINDOW SIZE:::::::::::::", window_width, window_height)
 
 function utils.px_to_xy(px)
 	local x = math.floor(px[1] / width) + 1
-	local y = arr_coords[math.floor(px[2] / height) + 1]
+	local y = utils.arr_coords[math.floor(px[2] / height) + 1]
 	print(y, x)
 	return vmath.vector({y, x})
 end
