09780c6d911b6686853dd8fd92c6da277446f362
Mohamed El Bakry <47481530+MohamedElBakry@users.noreply.github.com>
2021-04-15 12:36:38 +0100

    Implement knight move valid checking
    Also, comment clean up and an efficiency change -- Less data sent in msg.post from factory.script

--- piece/piece.lua
+++ piece/piece.lua
@@ -1,12 +1,17 @@
 require "utils-module.utils"
 
+-- This seems inefficient, so consider making more specific sub classes from Piece, e.g. Pawn, Knight, Queen ...
+-- Pawn = Piece:new()
+-- Pawn:isValid(move, array) ... 'specific validation'
+-- Knight = Piece:new()
+-- Knight:isValid(move, array) ... 'specific validation'
 
--- [[ Pawn Class --]]
+
+-- [[ Piece Class --]]
 
 -- Meta Class
 Piece = {id="", name="", pos={}, hasMoved=false}
 
--- ADD ID of GO
 -- Dervied Class
 function Piece:new(id, name, pos, hasMoved)
 	
@@ -21,21 +26,19 @@ function Piece:new(id, name, pos, hasMoved)
 	self.pos = pos
 	self.hasMoved = hasMoved
 	self.prevPos = {}
-	
 	-- self.valid_moves = {}
 
-	-- Knight: -15, -6, 10, 17, 15, 6, -10, -17
-	-- -17, -15, -10, -6, 6, 10, 15, 17
-	
 	return self
 end
 
 function Piece:move(pos)
+	self.prevPos = self.pos
 	self.pos = pos
 	self.hasMoved = true
 end
 
-function Piece:get_pos()
+-- Get a 'representation' of the position
+function Piece:get_repr_pos()
 	return "(" .. self.pos[1] .. ", " .. self.pos[2] .. ")"
 end
 
@@ -43,18 +46,20 @@ function Piece:set_pos(pos)
 	self.pos = pos
 end
 
--- White Pawn taking pattern: 9 or 7 and opposing colour piece present there
--- Black Pawn taking pattern: -9 or -7 and as above
 function Piece:isValid(move, array)
 	local valid = false
 	local flag = nil
 
 	local diff = utils.get_equivalent(self.pos[1], self.pos[2]) - utils.get_equivalent(move[1], move[2])
 	local landing_square_or_piece = array[move[1]][move[2]]
-	
+
+	-- TODO table system instead of 'if' chain so, valid, flag = __isvalid[self.piece](args)
 	-- Pawn
 	if self.piece == "p" then
 		valid, flag = self:__isvalid_pawn(move, array, diff, landing_square_or_piece)
+		
+	elseif self.piece == "n" then
+		valid, flag = self:__isvalid_knight(move, array, diff, landing_square_or_piece)
 	end
 	-- Knight
 	-- Etc.
@@ -72,17 +77,17 @@ function Piece:__isvalid_pawn(move, array, diff, landing_square_or_piece)
 		forward_one=8 
 	}
 
+	-- Negate the black pawn moves 
 	if self.colour == "b" then
 		for k, v in pairs(moves) do
 			moves[k] = -v
 		end
 	end
-
-	-- Compare current pos with new desired pos for capture patterns
-	-- If piece present in capture pattern square, then remove that piece -- IN MAIN?
-
+	
 	-- Capture check
-	-- If there's a piece present of the opposite colour, then it's valid
+	-- White Pawn taking pattern: 9 or 7 and opposing colour piece present there
+	-- Black Pawn taking pattern: -9 or -7 and as above
+	-- Compare current pos with new desired pos for capture patterns
 	if landing_square_or_piece ~= nil then
 		if diff == moves.capture_right or diff == moves.capture_left and landing_square_or_piece.colour ~= self.colour then
 			valid = true
@@ -104,11 +109,45 @@ function Piece:__isvalid_pawn(move, array, diff, landing_square_or_piece)
 	return valid, flag
 end
 
-function __isvalid_knight(move, array)
+-- Knight Move Valid Checking
+-- Knight: -15, -6, 10, 17, 15, 6, -10, -17
+-- -17, -15, -10, -6, 6, 10, 15, 17
+function Piece:__isvalid_knight(move, array, diff, landing_square_or_piece)
 	local valid = false
 	local flag = nil
+	
+	-- The 'polarity' i.e. positive or negative doesn't matter here
+	diff = math.abs(diff)
+
+	if diff == 17 or diff == 15 or diff == 10 or diff == 6 then
+		valid = true
+		if landing_square_or_piece ~= nil then
+			if landing_square_or_piece.colour ~= self.colour then
+				flag = "capture"
+			else
+				valid = false
+				flag = nil
+			end
+		end
+	end
+
 	return valid, flag
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
