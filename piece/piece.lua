c08d268b2032dccd407352dffa73585f3dc18ef3
Mohamed El Bakry <47481530+MohamedElBakry@users.noreply.github.com>
2021-04-15 11:04:51 +0100

    Implement pawn capture and movement for black and white
    Capture includes disabling a the captured piece, making it disappear

--- piece/piece.lua
+++ piece/piece.lua
@@ -1,5 +1,6 @@
 require "utils-module.utils"
 
+
 -- [[ Pawn Class --]]
 
 -- Meta Class
@@ -37,64 +38,54 @@ function Piece:set_pos(pos)
 	self.pos = pos
 end
 
--- TODO this
 -- White Pawn taking pattern: 9 or 7 and opposing colour piece present there
 -- Black Pawn taking pattern: -9 or -7 and as above
 function Piece:isValid(move, array)
 	local valid = false
+	local flag = nil
+
+	local diff = utils.get_equivalent(self.pos[1], self.pos[2]) - utils.get_equivalent(move[1], move[2])
+	local landing_square_or_piece = array[move[1]][move[2]]
 	
 	-- Pawn
-	-- If White
-	
 	if self.piece == "p" then
-		if self.colour == "w" then
-			
-			-- Compare current pos with new desired pos for capture patterns
-			-- If piece present in capture pattern square, then remove that piece -- IN MAIN?
-			local diff = utils.get_equivalent(self.pos[1], self.pos[2]) - utils.get_equivalent(move[1], move[2])
-			local landing_square_or_piece = array[move[1]][move[2]]
+		
+		moves = { 
+			capture_left=9, 
+			capture_right=7,
+			forward_one=8 
+		}
 
-			-- Capture check
-			if landing_square_or_piece ~= nil then
-				if diff == 9 or diff == 7 and landing_square_or_piece.colour ~= self.colour then
-					valid = true
-				end
+		if self.colour == "b" then
+			for k, v in pairs(moves) do
+				moves[k] = -v
 			end
-
-			-- Regular move check
-			-- 1 square forward
-			if diff == 8 and landing_square_or_piece == nil then
-				valid = true
-			-- Movement patterns +16 White, -16 Black if it's the first move, otherwise +/- 8
-			-- First move can be 2 squares forward or 1 square
-			elseif self.hasMoved == false and diff == 16 then
-				valid = true
-			end
-			
 		end
-	end
-	-- If Black
 	
-	return valid
+		-- Compare current pos with new desired pos for capture patterns
+		-- If piece present in capture pattern square, then remove that piece -- IN MAIN?
+
+		-- Capture check
+		-- If there's a piece present of the opposite colour, then it's valid
+		if landing_square_or_piece ~= nil then
+			if diff == moves.capture_right or diff == moves.capture_left and landing_square_or_piece.colour ~= self.colour then
+				valid = true
+				flag = "capture"
+			end
+		end
+
+		-- Regular move check
+		if diff == moves.forward_one and landing_square_or_piece == nil then
+			valid = true
+		-- Movement patterns +16 White, -16 Black if it's the first move, otherwise +/- 8
+		-- First move can be 2 squares forward or 1 square
+		elseif self.hasMoved == false and diff == moves.forward_one * 2 then
+			valid = true
+		end	
+	end
+
+	-- En passant
+	-- Diagonal capture behind a pawn directly to the left or right, that just moved 2 squares forward
+	
+	return valid, flag
 end
-
--- p = Piece:new(nil, "hash bla bla", "pw", {6, 3}, false)
--- pprint(p.pos)
--- pprint(p["pos"])
--- print(p:get_pos())
--- 
--- p:set_pos({6, 5})
--- pprint(p.pos, p["pos"], p:get_pos())
-
-
--- print(p:get_pos())
--- 
--- p:set_pos({7, 6})
--- print(p:get_pos())
--- p:move({7, 7})
--- print(p.name, p.hasMoved)
-
--- Example Usage
--- pawn = Piece:new(nil, "pw", {6, 1})
-
-
